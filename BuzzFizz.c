4be676901f8e0d83b918654c9b03fdb2f2dd2b9e
wxnguyen <nguyen.william@hotmail.com>
2016-03-11 22:53:43 -0800

    n checked for primality to avoid unnecessary execution of isPrime(Fn)

--- BuzzFizz.c
+++ BuzzFizz.c
@@ -8,30 +8,39 @@
 #define FALSE 0
 
 
-// Function to generate Fibonacci sequence and print messages
+// Function to generate nth Fibonacci number and print messages
 // dependent upon divisibility and primality of F(n)
 int fib(int n) {
-   int Fn, a, tmp;
+   int Fn, i, a, tmp;
 
    if (n >= 0 && n <= 46) {
       // Determine value of F(n), where 0 <= n <= 46
       Fn = 0; // F(0)
       a = 1;
-      while (n-- > 0) {
+      i = n;
+      while (i-- > 0) {
          tmp = Fn;
          Fn += a;
          a = tmp;
       }
 
-      // Determine divisibility or primality of F(n)
-      if (Fn % 3 == 0) {
-          printf("Buzz\n");
-          if (Fn % 5 == 0) {
-              printf("Fizz\n"); // Divisibility by both 3 and 5
-          }
-      } else if (Fn % 5 == 0) {
-          printf("Fizz\n");
-      } else if (isPrime(Fn)) {
+      // F(n) is divisible by F(k) if n is a multiple of k. Thus
+      // divisibility by 3 and 5 can be determined without calculating F(n).
+      // Since F(4) = 3, if n is divisible by 4, F(n) is divisible by 3.
+      if (n % 4 == 0) { //
+         printf("Buzz\n");
+         // Since F(5) = 5, if n is divisible by 5, F(n) is divisible by 5.
+         if (n % 5 == 0) {
+            printf("Fizz\n"); // Divisibility by both 3 and 5
+         }
+      } else if (n % 5 == 0) {
+         printf("Fizz\n");
+      // When F(n) is prime, n is also prime.
+      // For the current implementation of isPrime(), it is more efficient
+      // to check for primality of n since n is orders of magnitude smaller
+      // than F(n) when n is large. If n is not prime, the if statement will
+      // short-circuit and isPrime(Fn) will not be evaluated.
+      } else if (isPrime(n) && isPrime(Fn)) {
          printf("BuzzFizz\n");
       } else {
          printf("%d\n", Fn);
